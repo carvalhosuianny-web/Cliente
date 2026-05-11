30e6fb929a4dcc3322140eac548153901a7ef120
carvalhosuianny-web <carvalhosuianny@gmail.com>
2026-05-11 09:29:47 -0300

    Corrigindo deploy

--- cliente/settings.py
+++ cliente/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^mtsdkjem=v^_oqr+di69+hm)0=q=gx-^mc#po#-e-8j$(=e&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOMED_HOSTS = ["*"]
+ALLOMED_HOSTS = ["cliente-qr48.onrender.com"]
 
 LOGIN_URL = '/login/'
 
