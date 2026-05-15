fdba9baffc90a6965bd2b2e662633e0353bf20a4
carvalhosuianny-web <carvalhosuianny@gmail.com>
2026-05-15 08:32:45 -0300

    subir pra nuvem

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
@@ -97,5 +97,10 @@ def novo_usuario(request):
         form = UsuarioForm()
     context = {"form": form}
     return render(request, template_name, context)
+
+@login_required
+def sair(request):
+    logout(request)
+    return redirect('login_usuario')
         
         
